e1957e01f05ea97f6bf72fcef4f823836ff3dcf9
jeffcc <jeffcc@live.unc.edu>
2018-12-04 19:38:31 -0500

    stuff

--- final.js
+++ final.js
@@ -9,14 +9,14 @@ $(document).ready(function(){
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
 function dropdownFunction() {
-    document.getElementById("dropdown-content").classList.toggle("show");
+    document.getElementById("dropdown1").classList.toggle("show");
 }
 
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(event) {
   if (!event.target.matches('.dropbtn')) {
 
-    var dropdowns = document.getElementsById("dropdown-content");
+    var dropdowns = document.getElementsByClassName("dropdown-content");
     var i;
     for (i = 0; i < dropdowns.length; i++) {
       var openDropdown = dropdowns[i];
